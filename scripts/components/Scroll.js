9938855d675416ef06cf76e85267623b6a542639
kiars1 <DeadHaze317@yandex.ru>
2023-09-11 18:59:44 +0700

    scroll before refresh fix

--- scripts/components/Scroll.js
+++ scripts/components/Scroll.js
@@ -1,6 +1,4 @@
-import {
-  main,
-} from "../utils/constants.js";
+import { main } from "../utils/constants.js";
 
 export class Scroll {
   constructor(item) {
@@ -11,48 +9,61 @@ export class Scroll {
     const itemOffset = this._item.offsetTop;
     const itemHeight = this._item.offsetHeight;
     const animPoint = window.innerHeight - itemHeight;
+    const scroll = window.scrollY;
+    const vision = this._item.classList.contains("anim");
+    const mainHeight = main.offsetHeight;
 
-    if (this._item.classList.contains("main__background")) {
-      this._item.style = "";
-      this._item.classList.add("anim");
-    } else if (this._item.classList.contains("main__button")) {
-      this._item.style.right = `0px`;
-      this._item.classList.add("anim");
+    function animationScroll(thisitem, scroll, vision, mainHeight) {
+
+      if (thisitem.classList.contains("main__button")) {
+        thisitem.style.right = `0px`;
+        thisitem.classList.add("anim");
+      };
+
+      if (thisitem.classList.contains("header")) {
+        if (scroll >= mainHeight) {
+          thisitem.classList.add("header__down");
+          thisitem.style.transform = "initial";
+        } else if (scroll < 200) {
+          thisitem.classList.remove("header__down");
+          thisitem.style.transform = `none`;
+        } else {
+          thisitem.style.transform = `translateY(-100%)`;
+        }
+      } else if (thisitem.classList.contains("_top")) {
+        if (scroll >= itemOffset - animPoint && !vision) {
+          thisitem.style.top = `0px`;
+          thisitem.classList.add("anim");
+        }
+      } else if (thisitem.classList.contains("_left")) {
+        if (
+          scroll >= itemOffset - animPoint &&
+          !vision &&
+          thisitem.classList.contains("main__background")
+        ) {
+          thisitem.style.left = null;
+          thisitem.classList.add("anim");
+        } else if (
+          scroll >= itemOffset - animPoint / 1.5 &&
+          !vision &&
+          thisitem.classList.contains("installation__box")
+        ) {
+          thisitem.style.left = `0px`;
+          thisitem.classList.add("anim");
+        } else if (scroll >= itemOffset - animPoint && !vision) {
+          thisitem.style.left = `0px`;
+          thisitem.classList.add("anim");
+        }
+      }
     }
+
+    animationScroll(this._item, scroll, vision, mainHeight);
 
     window.addEventListener("scroll", () => {
       const scroll = window.scrollY;
       const vision = this._item.classList.contains("anim");
       const mainHeight = main.offsetHeight;
-
-      if (this._item.classList.contains("header")) {
-        if (scroll >= mainHeight) {
-          this._item.classList.add("header__down");
-          this._item.style.transform = "initial";
-        } else if (scroll < 200) {
-          this._item.classList.remove("header__down");
-          this._item.style.transform = `none`;
-        } else {
-          this._item.style.transform = `translateY(-100%)`;
-        }
-      } else if (this._item.classList.contains("_top")) {
-        if (scroll >= itemOffset - animPoint && !vision) {
-          this._item.style.top = `0px`;
-          this._item.classList.add("anim");
-        }
-      } else if (this._item.classList.contains("_left")) {
-        if (
-          scroll >= itemOffset - animPoint / 1.5 &&
-          !vision &&
-          this._item.classList.contains("installation__box")
-        ) {
-          this._item.style.left = `0px`;
-          this._item.classList.add("anim");
-        } else if (scroll >= itemOffset - animPoint && !vision) {
-          this._item.style.left = `0px`;
-          this._item.classList.add("anim");
-        }
-      }
+      animationScroll(this._item, scroll, vision, mainHeight);
     });
   }
 }
